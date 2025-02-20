60ef9247b25e43d38b58fd823e9bb2f2691242c4
rmgpanw <rmgpanw@ucl.ac.uk>
2025-02-20 08:36:49 +0000

    update get_nhsbsa_snomed_bnf()

--- R/lookups_and_mappings.R
+++ R/lookups_and_mappings.R
@@ -543,7 +543,7 @@ get_nhsbsa_snomed_bnf <- function(path = file.path(
                                     "bnf_dmd.zip"
                                   )) {
   download_file(
-    download_url = "https://www.nhsbsa.nhs.uk/sites/default/files/2024-04/BNF%20Snomed%20Mapping%20data%2020240423.zip",
+    download_url = "https://www.nhsbsa.nhs.uk/sites/default/files/2025-01/BNF%20Snomed%20Mapping%20data%2020250120.zip",
     path = path
   )
 }
