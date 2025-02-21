8dde46665f51a150e1afc2fb859616e0f79d6a75
rmgpanw <rmgpanw@ucl.ac.uk>
2025-02-21 06:45:08 +0000

    update and reorganise test-codeminer_qbr_translation.R

--- tests/testthat/test-codeminer_qbr_translation.R
+++ tests/testthat/test-codeminer_qbr_translation.R
@@ -1,83 +1,123 @@
-test_that("codeminer to qbr list translation works", {
 
-  # Simple queries - DESCRIPTION, CODES, CHILDREN ------------------------------
-  simple_query <- 'DESCRIPTION("diab") %AND% DESCRIPTION("retin|mac") %NOT%
+# Example codeminer query-qbr pairs ---------------------------------------
+
+egs <- list(
+  simple_query = list(
+    query = 'DESCRIPTION("diab") %AND% DESCRIPTION("retin|mac") %NOT%
      DESCRIPTION("absent|without") %OR% (DESCRIPTION("diab") %AND%
-     DESCRIPTION("nephro|neuro") %NOT% DESCRIPTION("absent|without"))'
-
-  qbr_from_simple_query <- translate_codeminer_query_to_qbr_list(simple_query)
-
-  expect_equal(qbr_from_simple_query,
-               list(
-                 condition = "OR",
-                 rules = list(
-                   list(condition = "NOT", rules = list(
-                     list(condition = "AND", rules = list(
-                       list(
-                         id = "description",
-                         field = "description",
-                         type = "string",
-                         input = "text",
-                         operator = "sct",
-                         value = "diab"
-                       ),
-                       list(
-                         id = "description",
-                         field = "description",
-                         type = "string",
-                         input = "text",
-                         operator = "sct",
-                         value = "retin|mac"
-                       )
-                     )),
-                     list(
-                       id = "description",
-                       field = "description",
-                       type = "string",
-                       input = "text",
-                       operator = "sct",
-                       value = "absent|without"
-                     )
-                   )),
-                   list(condition = "NOT", rules = list(
-                     list(condition = "AND", rules = list(
-                       list(
-                         id = "description",
-                         field = "description",
-                         type = "string",
-                         input = "text",
-                         operator = "sct",
-                         value = "diab"
-                       ),
-                       list(
-                         id = "description",
-                         field = "description",
-                         type = "string",
-                         input = "text",
-                         operator = "sct",
-                         value = "nephro|neuro"
-                       )
-                     )),
-                     list(
-                       id = "description",
-                       field = "description",
-                       type = "string",
-                       input = "text",
-                       operator = "sct",
-                       value = "absent|without"
-                     )
-                   ))
-                 ),
-                 valid = TRUE
-               ))
-
-  # Simple query, single statement -------------------------------------------
-
-  simple_query_single <- 'DESCRIPTION("diab")'
-
-  qbr_from_simple_query_single <- translate_codeminer_query_to_qbr_list(simple_query_single)
-
-  expect_equal(qbr_from_simple_query_single, list(
+     DESCRIPTION("nephro|neuro") %NOT% DESCRIPTION("absent|without"))' |>
+      stringr::str_squish(),
+    qbr = list(
+      condition = "OR",
+      rules = list(
+        list(condition = "NOT", rules = list(
+          list(condition = "AND", rules = list(
+            list(
+              id = "description",
+              field = "description",
+              type = "string",
+              input = "text",
+              operator = "sct",
+              value = "diab"
+            ),
+            list(
+              id = "description",
+              field = "description",
+              type = "string",
+              input = "text",
+              operator = "sct",
+              value = "retin|mac"
+            )
+          )),
+          list(
+            id = "description",
+            field = "description",
+            type = "string",
+            input = "text",
+            operator = "sct",
+            value = "absent|without"
+          )
+        )),
+        list(condition = "NOT", rules = list(
+          list(condition = "AND", rules = list(
+            list(
+              id = "description",
+              field = "description",
+              type = "string",
+              input = "text",
+              operator = "sct",
+              value = "diab"
+            ),
+            list(
+              id = "description",
+              field = "description",
+              type = "string",
+              input = "text",
+              operator = "sct",
+              value = "nephro|neuro"
+            )
+          )),
+          list(
+            id = "description",
+            field = "description",
+            type = "string",
+            input = "text",
+            operator = "sct",
+            value = "absent|without"
+          )
+        ))
+      ),
+      valid = TRUE
+    )
+  ),
+  simple_query2 = list(
+    query = 'DESCRIPTION("diab") %AND% DESCRIPTION("type 1") %OR%
+    (CHILDREN("E10") %OR% CODES("O240"))' |>
+      stringr::str_squish(),
+    qbr = list(
+      condition = "OR",
+      rules = list(
+        list(condition = "AND", rules = list(
+          list(
+            id = "description",
+            field = "description",
+            type = "string",
+            input = "text",
+            operator = "sct",
+            value = "diab"
+          ),
+          list(
+            id = "description",
+            field = "description",
+            type = "string",
+            input = "text",
+            operator = "sct",
+            value = "type 1"
+          )
+        )),
+        list(condition = "OR", rules = list(
+          list(
+            id = "children",
+            field = "children",
+            type = "string",
+            input = "text",
+            operator = "sct",
+            value = "E10"
+          ),
+          list(
+            id = "codes",
+            field = "codes",
+            type = "string",
+            input = "text",
+            operator = "sct",
+            value = "O240"
+          )
+        ))
+      ),
+      valid = TRUE
+    )
+  ),
+  simple_query_single = list(query = 'DESCRIPTION("diab")', qbr = list(
     list(
       id = "description",
       field = "description",
@@ -87,4 +127,38 @@ test_that("codeminer to qbr list translation works", {
       value = "diab"
     )
   ))
+)
+
+# TESTS -------------------------------------------------------------------
+
+test_that("Simple queries with AND/NOT/OR operators and DESCRIPTION/CHILDREN/CODES translate", {
+
+  # DESCRIPTION() only
+  expect_equal(translate_codeminer_query_to_qbr_list(egs$simple_query$query),
+               egs$simple_query$qbr)
+
+  expect_equal(egs$simple_query$query,
+               egs$simple_query$qbr |>
+                 custom_qbr_translation() |>
+                 deparse1())
+
+  # DESCRIPTION(), CHILDREN() and CODES()
+  expect_equal(translate_codeminer_query_to_qbr_list(egs$simple_query2$query),
+               egs$simple_query2$qbr)
+
+  expect_equal(egs$simple_query2$query,
+               egs$simple_query2$qbr |>
+                 custom_qbr_translation() |>
+                 deparse1())
+})
+
+test_that("Single statement single queries translate", {
+  expect_equal(translate_codeminer_query_to_qbr_list(egs$simple_query_single$query),
+               egs$simple_query_single$qbr)
+
+  expect_equal(egs$simple_query_single$query,
+               egs$simple_query_single$qbr |>
+                 custom_qbr_translation() |>
+                 deparse1())
+
 })
