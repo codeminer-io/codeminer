2b714461ecb283cade62ca07dab430d72bc0671d
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2025-10-21 15:07:09 +0100

    Remove shiny related files

--- inst/app/app.R
+++
@@ -1,8 +0,0 @@
-library(codeminer)
-library(gitcreds)
-options(shiny.maxRequestSize = 20 * 1024^2)
-
-build_all_lkps_maps() |>
-  all_lkps_maps_to_db()
-
-RunCodelistBuilder(all_lkps_maps = "all_lkps_maps.db")
