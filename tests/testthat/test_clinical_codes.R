ba2d9bfb7ac57cae1696e1d7afb76e006e64ce21
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2025-10-22 17:33:49 +0100

    Use dummy tables for tests

--- tests/testthat/test_clinical_codes.R
+++ tests/testthat/test_clinical_codes.R
@@ -3,17 +3,7 @@
 ukb_codings <- read_ukb_codings_dummy()
 
 all_lkps_maps_raw <- read_all_lkps_maps_dummy()
-all_lkps_maps <-
-  build_all_lkps_maps(
-    all_lkps_maps = all_lkps_maps_raw,
-    ukb_codings = ukb_codings,
-    bnf_dmd = NULL,
-    self_report_med_to_atc_map = NULL,
-    snomed_ct_uk_monolith = NULL,
-    phecode_1_2_lkp = NULL,
-    icd10_phecode_1_2 = NULL,
-    icd9_phecode_1_2 = NULL
-  )
+all_lkps_maps <- build_all_lkps_maps_dummy()
 
 all_lkps_maps_db <- all_lkps_maps_to_db(
   all_lkps_maps = all_lkps_maps,
