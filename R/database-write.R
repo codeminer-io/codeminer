f7ce01341f2df8399b441666eecd6a7a969c3762
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2025-10-29 20:48:56 +0000

    Add docs for `create_dummy_database`

--- R/database-write.R
+++ R/database-write.R
@@ -1,9 +1,19 @@
+#' Create a dummy database
+#'
+#' Sets up an example database for codeminer with dummy data
+#'
 #' @export
+#' @examples
+#' # Create dummy database in a temporary location
+#' temp_db <- tempfile(fileext = ".duckdb")
+#' Sys.setenv(CODEMINER_DB_PATH = temp_db)
+#' create_dummy_database()
 create_dummy_database <- function() {
-  example_tables <- utils::data("example_ontology")
-
-  lookup_table <- example_tables$lookup_tables$capital_letters_v3
-  lookup_metadata <- example_tables$lookup_metadata |>
+  # Workaround for the R CMD check note "no visible binding for global variable 'example_ontology'"
+  # and avoid loading it in the user's global environment
+  example_data <- codeminer::example_ontology
+  lookup_table <- example_data$lookup_tables$capital_letters_v3
+  lookup_metadata <- example_data$lookup_metadata |>
     dplyr::filter(.data$lookup_table_name == "capital_letters_v3")
 
   build_database()
