3c61ac685874e0595904f34be2b7a52adee1b46d
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2025-10-29 19:31:54 +0000

    Move db getters to utils

--- R/database-utils.R
+++ R/database-utils.R
@@ -2,3 +2,17 @@ table_exists <- function(con, tbl_name) {
   existing_tables <- DBI::dbListTables(con)
   return(tbl_name %in% existing_tables)
 }
+
+get_lookup_metadata <- function(con = connect_to_db()) {
+  tbl_name <- codeminer_metadata_table_names$lookup
+  get_table_from_db(con, tbl_name)
+}
+
+get_mapping_metadata <- function(con = connect_to_db()) {
+  tbl_name <- codeminer_metadata_table_names$mapping
+  get_table_from_db(con, tbl_name)
+}
+
+get_table_from_db <- function(con, tbl_name) {
+  DBI::dbReadTable(con, tbl_name)
+}
