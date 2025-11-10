680282152e3b301fdf0c90873c9f6a8405520715
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2025-11-10 17:13:07 +0100

    Unimplement `CHILDREN` for now

--- R/children.R
+++ R/children.R
@@ -1,3 +1,6 @@
+# TODO: to be implemented as part of
+# https://github.com/codeminer-io/codeminer/issues/44
+
 #' Get descendents for a code
 #'
 #' Retrieves children codes for a given set of codes (including the codes
@@ -12,7 +15,6 @@
 #'
 #' @return A data frame
 #' @name CHILDREN
-#' @export
 #'
 #' @seealso [get_children_sct()]
 #' @family Clinical code lookups and mappings
@@ -21,72 +23,66 @@
 CHILDREN <- function(
   codes,
   code_type = getOption("codeminer.code_type"),
-  all_lkps_maps = NULL,
-  codes_only = FALSE,
-  preferred_description_only = TRUE,
-  standardise_output = TRUE,
-  unrecognised_codes = "error",
-  col_filters = getOption("codeminer.col_filters")
+  version = getOption("codeminer.version", default = "latest"),
+  preferred_description_only = TRUE
 ) {
-  # check codes exist
-  codes <- CODES(
-    codes = codes,
-    code_type = code_type,
-    all_lkps_maps = all_lkps_maps,
-    preferred_description_only = TRUE,
-    standardise_output = TRUE,
-    unrecognised_codes = unrecognised_codes,
-    col_filters = col_filters,
-    .return_unrecognised_codes = FALSE
-  )
-
-  if (!is.null(codes)) {
-    code_type <- codes$code_type[1]
-
-    codes <- codes %>%
-      dplyr::pull(tidyselect::all_of("code")) %>%
-      unique()
-  } else {
-    return(codes)
-  }
-
-  # get child codes
-  if (code_type == "sct") {
-    get_children_sct(
-      codes = codes,
-      standardise_output = standardise_output,
-      include_self = TRUE,
-      include_descendants = TRUE,
-      all_lkps_maps = all_lkps_maps,
-      preferred_description_only = preferred_description_only,
-      col_filters = col_filters
-    )
-  } else if (
-    code_type %in%
-      c(
-        "bnf",
-        "icd9",
-        "icd10",
-        "read2",
-        "read2_drugs",
-        "opcs4",
-        "phecode"
-      )
-  ) {
-    codes_starting_with(
-      codes = codes,
-      code_type = code_type,
-      all_lkps_maps = all_lkps_maps,
-      codes_only = codes_only,
-      preferred_description_only = preferred_description_only,
-      standardise_output = standardise_output,
-      col_filters = col_filters,
-      escape_dot = FALSE
-    )
-  } else {
-    stop(paste0(
-      "Currently codeminer is unable to retrieve child codes for ",
-      code_type
-    ))
-  }
+  cli::cli_abort("Not implemented yet.")
+  # # check codes exist
+  # codes <- CODES(
+  #   codes = codes,
+  #   code_type = code_type,
+  #   all_lkps_maps = all_lkps_maps,
+  #   preferred_description_only = TRUE,
+  #   standardise_output = TRUE,
+  #   unrecognised_codes = unrecognised_codes,
+  #   col_filters = col_filters,
+  #   .return_unrecognised_codes = FALSE
+  # )
+  # if (!is.null(codes)) {
+  #   code_type <- codes$code_type[1]
+  #   codes <- codes %>%
+  #     dplyr::pull(tidyselect::all_of("code")) %>%
+  #     unique()
+  # } else {
+  #   return(codes)
+  # }
+  # # get child codes
+  # if (code_type == "sct") {
+  #   get_children_sct(
+  #     codes = codes,
+  #     standardise_output = standardise_output,
+  #     include_self = TRUE,
+  #     include_descendants = TRUE,
+  #     all_lkps_maps = all_lkps_maps,
+  #     preferred_description_only = preferred_description_only,
+  #     col_filters = col_filters
+  #   )
+  # } else if (
+  #   code_type %in%
+  #     c(
+  #       "bnf",
+  #       "icd9",
+  #       "icd10",
+  #       "read2",
+  #       "read2_drugs",
+  #       "opcs4",
+  #       "phecode"
+  #     )
+  # ) {
+  #   codes_starting_with(
+  #     codes = codes,
+  #     code_type = code_type,
+  #     all_lkps_maps = all_lkps_maps,
+  #     codes_only = codes_only,
+  #     preferred_description_only = preferred_description_only,
+  #     standardise_output = standardise_output,
+  #     col_filters = col_filters,
+  #     escape_dot = FALSE
+  #   )
+  # } else {
+  #   stop(paste0(
+  #     "Currently codeminer is unable to retrieve child codes for ",
+  #     code_type
+  #   ))
+  # }
 }
