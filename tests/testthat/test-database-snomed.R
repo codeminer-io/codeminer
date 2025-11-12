3d032150e069073ab0a16d4b7fb6909b22b257ef
Miguel Xochicale <m.xochicale@ucl.ac.uk>
2025-11-12 21:19:09 +0000

    use trud_metadata_for_1799item$releases[[1]]$id to avoid hardcode unit tests for latest version of snodmed

--- tests/testthat/test-database-snomed.R
+++ tests/testthat/test-database-snomed.R
@@ -3,27 +3,63 @@ withr::local_options(
   cli.default_handler = function(...) {}
 )
 
-test_that("download_latestversion_of_snomed_item() downloads the latest version of item 1799", {
-  # Skip if no TRUD_API_KEY is available
+test_that("download_latestversion_of_snomed_item() correctly downloads the latest version of SNOMED item 1799", {
+  # Unit tests are intended for local execution.
+  # See the function's documentation for prerequisites.
+
+  # Skip if no TRUD API key is available
+  skip_if(
+    Sys.getenv("TRUD_API_KEY") == "",
+    "TRUD_API_KEY environment variable not set, skipping test."
+  )
+
   skip_if(Sys.getenv("TRUD_API_KEY") == "", "No TRUD API key available")
 
   results <- download_latestversion_of_snomed_item(1799)
-  expect_equal(results$release_id, "uk_sct2mo_41.1.0_20251022000001Z.zip")
+
+  trud_metadata <- trud::get_item_metadata(1799, release_scope = "all")
+  latest_release_id <- trud_metadata$releases[[1]]$id
+
+  # Compare the downloaded release ID to the latest one in metadata
+  expect_equal(
+    results$release_id,
+    latest_release_id,
+    label = "Downloaded release ID",
+    expected.label = "Latest TRUD release ID"
+  )
 })
 
 test_that("read_snomed_ct_uk_monolith() returns appropriate tables", {
-  # Define path to the test data
-  path <- file.path(
+  # Unit tests are intended for local execution.
+  # See the function's documentation for prerequisites.
+
+  trud_metadata <- trud::get_item_metadata(1799, release_scope = "all")
+  expected_zip_name <- trud_metadata$releases[[1]]$archiveFileName
+  expected_base_name <- tools::file_path_sans_ext(expected_zip_name)
+
+  # Construct dynamic path with trud_metadata
+  expected_path <- file.path(
     getwd(),
-    "snomed_item_1799_uk_sct2mo_41.1.0_20251022000001Z.zip",
-    "SnomedCT_MonolithRF2_PRODUCTION_20251022T120000Z"
+    paste0("snomed_item_1799_", expected_zip_name),
+    paste0(
+      "SnomedCT_MonolithRF2_PRODUCTION_",
+      substr(
+        expected_base_name,
+        nchar(expected_base_name) - 14,
+        nchar(expected_base_name) - 7
+      ),
+      "T120000Z"
+    )
   )
 
   # Skip test for CI as path is for local test
-  skip_if(!dir.exists(path), paste("Test data is for local test:", path))
+  skip_if(
+    !dir.exists(expected_path),
+    paste("Test data is for local test:", expected_path)
+  )
 
   # Run the function
-  snomedct <- read_snomed_ct_uk_monolith(path)
+  snomedct <- read_snomed_ct_uk_monolith(expected_path)
 
   expect_equal(
     names(snomedct$sct_description),
