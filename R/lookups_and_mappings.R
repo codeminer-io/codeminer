7cc8530257af6c9f9e05a24a13bfbf237330c429
Miguel Xochicale <m.xochicale@ucl.ac.uk>
2025-11-13 12:29:57 +0000

    removes commented read_snomed_ct_uk_monolith

--- R/lookups_and_mappings.R
+++ R/lookups_and_mappings.R
@@ -1035,53 +1035,6 @@ extend_read_v2_drugs_bnf <- function(all_lkps_maps) {
   return(result)
 }
 
-# nolint start: commented_code_linter.
-# read_snomed_ct_uk_monolith() has been refactored in database-snomed.R
-# #' Read the SNOMED CT UK Monolith Edition into R
-# #'
-# #' @param snomed_ct_uk_monolith_dir Optional: path to the unzipped [SNOMED CT UK
-# #'   Monolith
-# #'   Edition](https://isd.digital.nhs.uk/trud/users/guest/filters/2/categories/26/items/1799/releases).
-# #'
-# #' @return A list.
-# #' @noRd
-# read_snomed_ct_uk_monolith <- function(snomed_ct_uk_monolith_dir) {
-#   # terminology and refset tables
-#   snomed_monolith_terminology <-
-#     file.path(snomed_ct_uk_monolith_dir, "Snapshot", "Terminology") %>%
-#     list.files(full.names = TRUE) %>%
-#     purrr::set_names(nm = fs::path_file) %>%
-#     purrr::map(~ data.table::fread(.x, sep = "\t", colClasses = "character"))
-
-#   snomed_monolith_refset <- file.path(
-#     snomed_ct_uk_monolith_dir,
-#     "Snapshot",
-#     "Refset"
-#   ) %>%
-#     list.files(full.names = TRUE) %>%
-#     purrr::set_names(nm = fs::path_file) %>%
-#     purrr::map(
-#       ~ .x %>%
-#         list.files(full.names = TRUE) %>%
-#         purrr::set_names(nm = fs::path_file) %>%
-#         purrr::map(
-#           ~ data.table::fread(
-#             .x,
-#             sep = "\t",
-#             colClasses = "character"
-#           )
-#         )
-#     )
-
-#   return(
-#     list(
-#       snomed_monolith_terminology = snomed_monolith_terminology,
-#       snomed_monolith_refset = snomed_monolith_refset
-#     )
-#   )
-# }
-# nolint end: commented_code_linter.
-
 #' Read the NHS Data Migration release into R
 #'
 #' @param nhs_data_migration_dir path to the unzipped [NHS Data
