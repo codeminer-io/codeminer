d09b81a696c4136120a5ef92fd1e752240b88a42
Milan Malfait <38256462+milanmlft@users.noreply.github.com>
2025-11-14 16:43:58 +0100

    Move over legacy code

--- R/children.R
+++ R/children.R
@@ -87,3 +87,112 @@ CHILDREN <- function(
   #   ))
   # }
 }
+
+#' Get children for SNOMED codes
+#'
+#' @param codes Character vector of SNOMED codes.
+#' @param standardise_output If `TRUE` (default) return a data frame with columns
+#'   'code', 'description' and 'code_type'.
+#' @param include_self If `TRUE` (default) include input codes in the result.
+#' @param include_descendants If `TRUE` (default) return all descendant codes,
+#'   as well as immediate children.
+#' @inheritParams CODES
+#' @inheritParams CHILDREN
+#'
+#' @return A dataframe
+#' @seealso [CHILDREN()], [get_parents_sct()]
+#' @family Clinical code lookups and mappings
+#' @export
+get_children_sct <- function(
+  codes,
+  standardise_output = TRUE,
+  include_self = TRUE,
+  include_descendants = TRUE,
+  preferred_description_only = TRUE
+) {
+  get_relatives_sct(
+    codes = codes,
+    filter_col = "destinationId",
+    return_col = "sourceId",
+    typeId = "116680003",
+    standardise_output = standardise_output,
+    include_self = include_self,
+    recursive = include_descendants,
+    preferred_description_only = preferred_description_only
+  )
+}
+
+get_relatives_sct <- function(
+  codes = NULL,
+  filter_col = "destinationId",
+  return_col = "sourceId",
+  typeId = "116680003",
+  standardise_output = TRUE,
+  include_self = TRUE,
+  recursive = TRUE,
+  all_lkps_maps = NULL,
+  preferred_description_only = TRUE,
+  col_filters = getOption("codeminer.col_filters")
+) {
+  match.arg(filter_col, choices = c("sourceId", "destinationId"))
+
+  # set up
+  sourceId_filter <- destinationId_filter <- NULL
+
+  if (filter_col == "sourceId") {
+    sourceId_filter <- codes
+  }
+
+  if (filter_col == "destinationId") {
+    destinationId_filter <- codes
+  }
+
+  # get codes from relationship table
+  input_codes <- filter_sct_relationship(
+    codes = NULL,
+    sourceId_filter = sourceId_filter,
+    destinationId_filter = destinationId_filter,
+    typeId_filter = typeId,
+    active_only = TRUE,
+    recursive = FALSE,
+    all_lkps_maps = all_lkps_maps
+  )
+
+  sourceId_filter <- destinationId_filter <- NULL
+
+  if (filter_col == "sourceId") {
+    sourceId_filter <- unique(input_codes$destinationId)
+  }
+
+  if (filter_col == "destinationId") {
+    destinationId_filter <- unique(input_codes$sourceId)
+  }
+
+  # perform search recursively
+  if (recursive) {
+    output_codes <- filter_sct_relationship(
+      codes = input_codes,
+      sourceId_filter = sourceId_filter,
+      destinationId_filter = destinationId_filter,
+      typeId_filter = typeId,
+      active_only = TRUE,
+      recursive = recursive,
+      all_lkps_maps = all_lkps_maps
+    )
+  } else {
+    output_codes <- input_codes
+  }
+
+  # lookup results
+  result <- unique(output_codes[[return_col]])
+
+  if (include_self) {
+    result <- c(codes, result)
+  }
+
+  CODES(
+    codes = result,
+    code_type = "sct",
+    preferred_description_only = preferred_description_only
+  )
+}
