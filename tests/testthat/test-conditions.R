50a3efc438f23b3b6a7b411ca156f585e22eb3ee
Alasdair Warwick <54263156+rmgpanw@users.noreply.github.com>
2025-11-21 16:36:59 +0000

    reformat code, tests/testthat/test-conditions.R
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- tests/testthat/test-conditions.R
+++ tests/testthat/test-conditions.R
@@ -6,12 +6,12 @@ test_that("codeminer_abort creates a CodeMiner error condition", {
 })
 
 test_that("codeminer_abort stores the original cli message vector in cli_error_message", {
-	e <- tryCatch(
-		codeminer_abort(c(x = "Test error.", i = "Extra info.")),
-		error = function(e) e
-	)
-	expect_true("cli_error_message" %in% names(e))
-	expect_equal(e$cli_error_message, c(x = "Test error.", i = "Extra info."))
+  e <- tryCatch(
+    codeminer_abort(c(x = "Test error.", i = "Extra info.")),
+    error = function(e) e
+  )
+  expect_true("cli_error_message" %in% names(e))
+  expect_equal(e$cli_error_message, c(x = "Test error.", i = "Extra info."))
 })
 
 test_that("codeminer_abort allows additional custom classes to be prepended", {
